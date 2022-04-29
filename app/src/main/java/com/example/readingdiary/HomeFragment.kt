57da6538f5565e410913f381f7b6867a7976949d
hisun <hisungwoo007@naver.com>
2022-04-29 21:33:38 +0900

    프래그먼트 및 메뉴 추가

---
+++ app/src/main/java/com/example/readingdiary/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.readingdiary
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+
+    //Category.newInstance()사용을 위해 생성
+    companion object {
+        fun newInstance() : HomeFragment {
+            return HomeFragment()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
