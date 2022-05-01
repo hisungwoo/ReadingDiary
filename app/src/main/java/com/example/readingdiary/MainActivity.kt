7809b2051867ebb2023834022096ccea3bad2ac5
hisun <hisungwoo007@naver.com>
2022-05-01 22:38:01 +0900

    viewModel 추가

--- app/src/main/java/com/example/readingdiary/MainActivity.kt
+++ app/src/main/java/com/example/readingdiary/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.readingdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+    val mainViewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
