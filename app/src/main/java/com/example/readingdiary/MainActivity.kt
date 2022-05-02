a5ec728bb60caa6082542ad9ff00b15a52e854ac
hisun <hisungwoo007@naver.com>
2022-05-02 21:34:39 +0900

    DataBinding 추가

--- app/src/main/java/com/example/readingdiary/MainActivity.kt
+++ app/src/main/java/com/example/readingdiary/MainActivity.kt
@@ -2,14 +2,31 @@ package com.example.readingdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import com.example.readingdiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     val mainViewModel : MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.i1 -> {
+                }
+                R.id.i2 -> {
+                }
+                R.id.i3 -> {
+                }
+                R.id.i4 -> {
+                }
+            }
+            true
+        }
 
     }
 }
