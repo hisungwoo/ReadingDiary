a58861930a45367e64d1229a3003407400a3540a
hisun <hisungwoo007@naver.com>
2022-05-05 04:12:12 +0900

    1. 프래그먼트 추가 2. 프래그먼트 이동 추가

--- app/src/main/java/com/example/readingdiary/MainActivity.kt
+++ app/src/main/java/com/example/readingdiary/MainActivity.kt
@@ -9,21 +9,30 @@ import com.example.readingdiary.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     val mainViewModel : MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
+    private val homeFragment by lazy { HomeFragment.newInstance() }
+    private val booksFragment by lazy { BooksFragment.newInstance() }
+    private val chartFragment by lazy { ChartFragment.newInstance() }
+    private val settingFragment by lazy { SettingFragment.newInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        supportFragmentManager.beginTransaction().add(R.id.main_fragment_view, homeFragment)
 
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.menu_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_fragment_view, homeFragment).commit()
                 }
                 R.id.menu_books -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_fragment_view, booksFragment).commit()
                 }
                 R.id.menu_chart -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_fragment_view, chartFragment).commit()
                 }
                 R.id.menu_setting -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_fragment_view, settingFragment).commit()
                 }
             }
             true
