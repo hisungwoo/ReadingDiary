52a48df8750157a1722df6d131ae552d59e69258
hisun <hisungwoo007@naver.com>
2022-05-07 22:50:34 +0900

    1.recyclearView 추가 2. BooksAdapter 추가

---
+++ app/src/main/java/com/example/readingdiary/BooksAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.readingdiary
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BooksAdapter : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+
+    inner class BooksViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ???, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
