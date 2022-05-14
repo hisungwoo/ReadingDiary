22e96f1c5c6d9dab35390f222a6dc6b40d836553
hisun <hisungwoo007@naver.com>
2022-05-14 14:01:20 +0900

    1.Jetpack Navigation 추가 2.Bottom Nav, Jetpack Navi 연동

--- app/src/main/java/com/ilsamil/readingdiary/MainActivity.kt
+++ app/src/main/java/com/ilsamil/readingdiary/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ilsamil.readingdiary.fragments.BooksFragment
 import com.ilsamil.readingdiary.fragments.ChartFragment
 import com.ilsamil.readingdiary.fragments.HomeFragment
@@ -14,94 +19,71 @@ import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val TAG = "MainActivity"
+    }
+
     val mainViewModel : MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
     private val homeFragment by lazy { HomeFragment.newInstance() }
     private val booksFragment by lazy { BooksFragment.newInstance() }
     private val chartFragment by lazy { ChartFragment.newInstance() }
     private val settingFragment by lazy { SettingFragment.newInstance() }
+    lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        val retrofit = Retrofit.Builder()
-                    .baseUrl("https://dapi.kakao.com")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-
-        val bookService = retrofit.create(BookService::class.java)
-
-
-        bookService.getBookInfo("미움받을용기", "accuracy", 50, "title")
-            .enqueue(object : Callback<SearchBookDto> {
-                override fun onResponse(
-                    call: Call<SearchBookDto>,
-                    response: Response<SearchBookDto>
-                ) {
-                    Log.d("ttest", " 성공!! ")
-
-                    if(response.isSuccessful.not()) {
-                        return
-                    }
-                    response.body()?.let {
-                        Log.d("ttest", "body 있음")
-
-                        it.bookInfo.forEach { books ->
-                            Log.d("ttest", books.toString())
-                        }
-
-                    }
-
-                }
-
-                override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
-                    Log.d("ttest",t.toString())
-
-                }
-
-
-            })
-
-
-
-
-
-
-
-
-
-
-
-
-        supportFragmentManager.beginTransaction().add(R.id.main_fragment_view, homeFragment)
-
-
-
-
-
-
-
-        binding.bottomNav.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.menu_home -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.main_fragment_view, homeFragment).commit()
-                }
-                R.id.menu_books -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.main_fragment_view, booksFragment).commit()
-                }
-                R.id.menu_chart -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.main_fragment_view, chartFragment).commit()
-                }
-                R.id.menu_setting -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.main_fragment_view, settingFragment).commit()
-                }
-            }
-            true
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host)
+        if (navHostFragment != null) {
+            navController = navHostFragment.findNavController()
         }
+
+        binding.bottomNav.setupWithNavController(navController)
+
+//        val retrofit = Retrofit.Builder()
+//                    .baseUrl("https://dapi.kakao.com")
+//                    .addConverterFactory(GsonConverterFactory.create())
+//                    .build()
+//
+//        val bookService = retrofit.create(BookService::class.java)
+
+
+//        bookService.getBookInfo("미움받을용기", "accuracy", 50, "title")
+//            .enqueue(object : Callback<SearchBookDto> {
+//                override fun onResponse(
+//                    call: Call<SearchBookDto>,
+//                    response: Response<SearchBookDto>
+//                ) {
+//                    Log.d(TAG, " 성공!! ")
+//
+//                    if(response.isSuccessful.not()) {
+//                        return
+//                    }
+//                    response.body()?.let {
+//                        Log.d(TAG, "body 있음")
+//
+//                        it.bookInfo.forEach { books ->
+//                            Log.d(TAG, books.toString())
+//                        }
+//
+//                    }
+//
+//                }
+//                override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
+//                    Log.d("ttest",t.toString())
+//
+//                }
+//            })
+
+
+
+
 
         setContentView(binding.root)
     }
+
 
 
 }
--- app/src/main/java/com/ilsamil/readingdiary/fragments/HomeFragment.kt
+++ app/src/main/java/com/ilsamil/readingdiary/fragments/HomeFragment.kt
@@ -2,13 +2,20 @@ package com.ilsamil.readingdiary.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.ilsamil.readingdiary.R
+import com.ilsamil.readingdiary.databinding.ActivityMainBinding
+import com.ilsamil.readingdiary.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     //Category.newInstance()사용을 위해 생성
     companion object {
@@ -29,7 +36,15 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.moveBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_booksFragment)
+        }
     }
 
 }
