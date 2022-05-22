f0b66ca7a8d893cd2ae66e67a56f6d6eb05d6ff5
hisun <hisungwoo007@naver.com>
2022-05-22 12:27:13 +0900

    recyclear를 사용하여 calendar 추가

--- app/src/main/java/com/ilsamil/readingdiary/adapter/CalendarAdapter.kt
+++ app/src/main/java/com/ilsamil/readingdiary/adapter/CalendarAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -14,112 +15,26 @@ import com.ilsamil.readingdiary.databinding.DayBinding
 import com.ilsamil.readingdiary.databinding.EmptyDayBinding
 import java.util.*
 
-class CalendarAdapter(calendarList : List<Any>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private val HEADER_TYPE = 0
-    private val EMPTY_TYPE = 1
-    private val DAY_TYPE = 2
+class CalendarAdapter(calendarList : ArrayList<String>) : RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
+    var dayList : ArrayList<String> = calendarList
 
-    private var mCalendarList = calendarList
+    inner class CalendarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var dayText : TextView = itemView.findViewById(R.id.item_dayText_tv)
+    }
 
-    fun setCalendarList(calendarList : List<Any>) {
-        mCalendarList = calendarList
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_calendar, parent, false)
+        return CalendarViewHolder(view)
     }
 
 
-    override fun getItemViewType(position: Int): Int {
-        var item = mCalendarList[position]
-        return if(item is Long) {
-            //날짜 타입
-            HEADER_TYPE
-        } else if (item is String) {
-            //비어있는 일자 타입
-            EMPTY_TYPE
-        } else {
-            DAY_TYPE
-        }
-//        when(item) {
-//            is Long -> HEADER_TYPE
-//            is String -> EMPTY_TYPE
-//            else -> DAY_TYPE
-//        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if (viewType == HEADER_TYPE) {
-            val binding =
-                DataBindingUtil.inflate<CalendarHeaderBinding>(LayoutInflater.from(parent.context), R.layout.item_calendar_header, parent, false)
-            val params : StaggeredGridLayoutManager.LayoutParams = binding.root.layoutParams as StaggeredGridLayoutManager.LayoutParams
-            binding.root.layoutParams = params
-            return HeaderViewHolder(binding)
-        } else if (viewType == EMPTY_TYPE) {
-            val binding =
-                DataBindingUtil.inflate<EmptyDayBinding>(LayoutInflater.from(parent.context), R.layout.item_calendar_header, parent, false)
-            return EmptyViewHolder(binding)
-        } else {
-            val binding =
-                DataBindingUtil.inflate<DayBinding>(LayoutInflater.from(parent.context), R.layout.item_calendar_header, parent, false)
-            return DayViewHolder(binding)
-        }
-    }
-
-    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
-        val viewType = getItemViewType(position)
-        if (viewType == HEADER_TYPE) {
-            val holder = viewHolder as HeaderViewHolder
-            val item = mCalendarList[position]
-            val model = MainViewModel()
-            if (item is Long) {
-                model.setHeaderDate(item as Long)
-            }
-            holder.setViewModel(model)
-        } else if (viewType == EMPTY_TYPE) {
-            val holder = viewHolder as EmptyViewHolder
-            val model = MainViewModel()
-            holder.setViewModel(model)
-        } else if (viewType == DAY_TYPE) {
-            val holder = viewHolder as DayViewHolder
-            val item = mCalendarList[position]
-            val model = MainViewModel()
-            if (item is Calendar) {
-                model.setCalendar(item as Calendar)
-            }
-            holder.setViewModel(model)
-        }
-
+    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
+        holder.dayText.text = dayList[position]
     }
 
     override fun getItemCount(): Int {
-        if(mCalendarList != null) {
-            return mCalendarList.size
-        }
-        return 0
+        return dayList.size
     }
 
 
-    inner class HeaderViewHolder(binding : CalendarHeaderBinding) : RecyclerView.ViewHolder(binding.root) {
-        private val binding : CalendarHeaderBinding = binding
-
-        fun setViewModel(model : MainViewModel) {
-            binding.model = model
-            binding.executePendingBindings()
-        }
-    }
-
-    inner class EmptyViewHolder(binding : EmptyDayBinding) : RecyclerView.ViewHolder(binding.root) {
-        private val binding : EmptyDayBinding = binding
-
-        fun setViewModel(model : MainViewModel) {
-            binding.model = model
-            binding.executePendingBindings()
-        }
-    }
-
-    inner class DayViewHolder(binding : DayBinding) : RecyclerView.ViewHolder(binding.root) {
-        private val binding : DayBinding = binding
-
-        fun setViewModel(model : MainViewModel) {
-            binding.model = model
-            binding.executePendingBindings()
-        }
-    }
 }
--- app/src/main/java/com/ilsamil/readingdiary/fragments/HomeFragment.kt
+++ app/src/main/java/com/ilsamil/readingdiary/fragments/HomeFragment.kt
@@ -15,6 +15,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.ilsamil.readingdiary.BR
 import com.ilsamil.readingdiary.MainViewModel
@@ -23,12 +26,18 @@ import com.ilsamil.readingdiary.adapter.CalendarAdapter
 import com.ilsamil.readingdiary.databinding.CalendarListBinding
 import java.lang.Exception
 import java.security.Key
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.collections.ArrayList
 
 class HomeFragment : Fragment() {
     private val mainViewModel by activityViewModels<MainViewModel>()
+    lateinit var binding : CalendarListBinding
     lateinit var calendarAdapter : CalendarAdapter
+
+    lateinit var selectedDate : LocalDate
 
     //Category.newInstance()사용을 위해 생성
     companion object {
@@ -52,7 +61,7 @@ class HomeFragment : Fragment() {
     ): View? {
 //        val binding : CalendarListBinding = CalendarListBinding.inflate(inflater, container, false)
 //        val binding = DataBindingUtil.inflate<FragmentHomeBinding>()
-        val binding = DataBindingUtil.inflate<CalendarListBinding>(inflater, R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         binding.setVariable(BR.model, ViewModelProvider(this).get(MainViewModel::class.java))
         binding.lifecycleOwner = this
         binding.model = mainViewModel
@@ -65,19 +74,107 @@ class HomeFragment : Fragment() {
 
 
 
+        //현재 날짜
+        selectedDate = LocalDate.now()
 
-        mainViewModel.mCalendarList.observe(this, androidx.lifecycle.Observer {
+        //달력 set
+        setCalendar()
+
+
+//        mainViewModel.mCalendarList.observe(this, androidx.lifecycle.Observer {
 //            val view = binding.pagerClaendar
-        })
+//        })
 
 
 
+        binding.calPrevBtn.setOnClickListener {
+            selectedDate = selectedDate.minusMonths(1)
+            setCalendar()
+        }
 
+
+        binding.calNextBtn.setOnClickListener {
+            selectedDate = selectedDate.plusMonths(1)
+            setCalendar()
+        }
 
 
         return binding?.root
     }
 
+    //날짜 타입 설정
+    private fun monthYearFromDate(date : LocalDate) : String {
+        val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월")
+        return date.format(formatter)
+    }
+
+    //화면 설정
+    private fun setMonthView() {
+        //년,월 텍스트 뷰 셋팅
+        binding.calCurrentDateTv.text = monthYearFromDate(selectedDate)
+    }
+
+    private fun daysInMonthArray(date : LocalDate) : ArrayList<String> {
+        var dayList = ArrayList<String>()
+        val yearMonth = YearMonth.from(date)
+
+        // 해당 월 마지막 일
+        val lastDay = yearMonth.lengthOfMonth()
+
+        // 해당 월 첫번째 날
+        val firstDay = selectedDate.withDayOfMonth(1)
+
+        // 첫번째 요일
+        val dayOfWeek = firstDay.dayOfWeek.value
+
+        for(i in 1 .. 42) {
+            if(i <= dayOfWeek || i > lastDay + dayOfWeek) {
+                dayList.add("")
+            } else {
+                dayList.add((i-dayOfWeek).toString())
+            }
+        }
+
+        Log.d("ttest", dayList.toString())
+
+        var isEmptyCheck = false
+        for(i in 0..6) {
+            Log.d("ttest", "i = " + dayList[i])
+            if(dayList[i] == "") {
+                isEmptyCheck = true
+            } else {
+                isEmptyCheck = false
+                break
+            }
+        }
+        if(isEmptyCheck) {
+            dayList.removeAt(0)
+            dayList.removeAt(0)
+            dayList.removeAt(0)
+            dayList.removeAt(0)
+            dayList.removeAt(0)
+            dayList.removeAt(0)
+            dayList.removeAt(0)
+        }
+
+        Log.d("ttest", dayList.toString())
+
+
+        return dayList
+    }
+
+    private fun setCalendar() {
+        //년,월 텍스트뷰 셋팅
+        setMonthView()
+
+        val dayList = daysInMonthArray(selectedDate)
+        val calendarAdapter = CalendarAdapter(dayList)
+        binding.calRecyclerview.layoutManager = GridLayoutManager(context,
+            7)
+        binding.calRecyclerview.adapter = calendarAdapter
+
+
+    }
 
 }
 
