aeec22f74a22369157eeaa73a5ea9a08aa5f4cc1
hisun <hisungwoo007@naver.com>
2022-06-30 16:17:30 +0900

    책장 디자인 변경

--- app/src/main/java/com/ilsamil/readingdiary/adapter/StatsAdapter.kt
+++ app/src/main/java/com/ilsamil/readingdiary/adapter/StatsAdapter.kt
@@ -1,18 +1,27 @@
 package com.ilsamil.readingdiary.adapter
 
+import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.ilsamil.readingdiary.R
+import java.util.*
+import kotlin.collections.ArrayList
 
 class StatsAdapter : RecyclerView.Adapter<StatsAdapter.StatsViewHolder>() {
     private var bookShelfList : List<String> = ArrayList()
+    private val colorArr = arrayOf(R.drawable.stats_book_item, R.drawable.stats_book_item2,
+                    R.drawable.stats_book_item3, R.drawable.stats_book_item4, R.drawable.stats_book_item5)
+    private val random = Random()
 
     inner class StatsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val statsNameTv : TextView = itemView.findViewById(R.id.item_stats_name_tv)
+        val statsView : View = itemView.findViewById(R.id.item_stats_book)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StatsViewHolder {
@@ -20,9 +29,16 @@ class StatsAdapter : RecyclerView.Adapter<StatsAdapter.StatsViewHolder>() {
         return StatsViewHolder(view)
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onBindViewHolder(holder: StatsViewHolder, position: Int) {
         val item = bookShelfList[position]
+        val colNum = random.nextInt(5)
+
+        Log.d("ttest","colNum = $colNum")
+
         holder.statsNameTv.text = item
+        holder.statsView.setBackgroundResource(colorArr[colNum])
+//        holder.statsView.setBackgroundColor(R.color.stats_item_color1)
     }
 
     override fun getItemCount(): Int {
@@ -33,4 +49,5 @@ class StatsAdapter : RecyclerView.Adapter<StatsAdapter.StatsViewHolder>() {
         bookShelfList = items
         notifyDataSetChanged()
     }
+
 }
